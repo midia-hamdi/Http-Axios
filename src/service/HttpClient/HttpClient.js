6b5748c1a7b79519809601a2f808c72c8ea7d7a0
midia-hamdi <midiahamdi367@gmail.com>
2023-12-26 13:37:37 +0330

    Http methods axios

---
+++ src/service/HttpClient/HttpClient.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.headers.common['Authorization'] = 555;
+
+export function get(url, config = {}){
+    return axios.get(url, config)
+        .then(response => response.data)
+        .catch(error => console.log(error))
+}
+
+export function post(url, data, config){
+    return axios.post(url, data, config)
+        .then(response => response.data)
+        .catch(error => console.log(error))
+    
+}
+
+export class HttpClient{
+    constructor(config = {}) {
+        this.servoce =  axios.create({
+            ...config,
+            baseURL: config.baseURL || process.env.REACT_APP_BASE_URL,
+        })
+    }
+
+    get(url, config = {}){
+        return new Promise((resolve, reject) => {
+            axios.get(url, config)
+                .then(response => resolve(response.data))
+                .catch(error => reject(error))
+        }) 
+    }
+
+    post(url, data, config){
+        return axios.post(url, data, config)
+        .then(response => response.data)
+        .catch(error => console.log(error))
+    }
+
+}
+
+
